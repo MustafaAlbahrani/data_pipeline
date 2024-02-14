ec84e7476aa8579d883ef668d74bf53b96172afc
MustafaAlbahrani <75665113+MustafaAlbahrani@users.noreply.github.com>
2024-02-14 23:16:59 +0300

    Add files via upload

--- create_tables.sql
+++ create_tables.sql
@@ -1,29 +1,16 @@
-DROP table IF EXISTS public.artists;
-
-DROP table IF EXISTS public.songplays;
-
-DROP table IF EXISTS public.songs;
-
-DROP table IF EXISTS public.staging_events;
-
-DROP table IF EXISTS public.staging_songs;
-
-DROP table IF EXISTS public."time";
-
-DROP table IF EXISTS public.users;
-
-CREATE TABLE public.artists (
+CREATE TABLE IF NOT EXISTS public.artists (
 	artistid varchar(256) NOT NULL,
-	name varchar(512),
-	location varchar(512),
+	name varchar(256),
+	location varchar(256),
 	lattitude numeric(18,0),
-	longitude numeric(18,0)
+    longitude numeric(18,0)
 );
 
-CREATE TABLE public.songplays (
-	playid varchar(32) NOT NULL,
-	start_time timestamp NOT NULL,
-	userid int4 NOT NULL,
+
+CREATE TABLE IF NOT EXISTS public.songplays (
+	playid varchar(32) ,
+	start_time timestamp ,
+	userid int4,
 	"level" varchar(256),
 	songid varchar(256),
 	artistid varchar(256),
@@ -33,16 +20,16 @@ CREATE TABLE public.songplays (
 	CONSTRAINT songplays_pkey PRIMARY KEY (playid)
 );
 
-CREATE TABLE public.songs (
+CREATE TABLE IF NOT EXISTS public.songs (
 	songid varchar(256) NOT NULL,
-	title varchar(512),
+	title varchar(256),
 	artistid varchar(256),
 	"year" int4,
 	duration numeric(18,0),
 	CONSTRAINT songs_pkey PRIMARY KEY (songid)
 );
 
-CREATE TABLE public.staging_events (
+CREATE TABLE IF NOT EXISTS public.staging_events (
 	artist varchar(256),
 	auth varchar(256),
 	firstname varchar(256),
@@ -63,31 +50,33 @@ CREATE TABLE public.staging_events (
 	userid int4
 );
 
-CREATE TABLE public.staging_songs (
+CREATE TABLE IF NOT EXISTS public.staging_songs (
 	num_songs int4,
 	artist_id varchar(256),
-	artist_name varchar(512),
+	artist_name varchar(256),
 	artist_latitude numeric(18,0),
 	artist_longitude numeric(18,0),
-	artist_location varchar(512),
+	artist_location varchar(256),
 	song_id varchar(256),
-	title varchar(512),
+	title varchar(256),
 	duration numeric(18,0),
 	"year" int4
 );
 
-CREATE TABLE public."time" (
-	start_time timestamp NOT NULL,
-	"hour" int4,
-	"day" int4,
-	week int4,
-	"month" varchar(256),
-	"year" int4,
-	weekday varchar(256),
-	CONSTRAINT time_pkey PRIMARY KEY (start_time)
-) ;
+CREATE TABLE IF NOT EXISTS public.staging_songs (
+	num_songs int4,
+	artist_id varchar(256),
+	artist_name varchar(256),
+	artist_latitude numeric(18,0),
+	artist_longitude numeric(18,0),
+	artist_location varchar(256),
+	song_id varchar(256),
+	title varchar(256),
+	duration numeric(18,0),
+	"year" int4
+);
 
-CREATE TABLE public.users (
+CREATE TABLE IF NOT EXISTS public.users (
 	userid int4 NOT NULL,
 	first_name varchar(256),
 	last_name varchar(256),
@@ -95,3 +84,18 @@ CREATE TABLE public.users (
 	"level" varchar(256),
 	CONSTRAINT users_pkey PRIMARY KEY (userid)
 );
+
+CREATE TABLE IF NOT EXISTS time (
+    start_time timestamp PRIMARY KEY,
+    hour varchar,
+    day varchar,
+    week varchar,
+    month varchar,
+    year varchar,
+    weekday varchar
+);
+
+
+
+
+
