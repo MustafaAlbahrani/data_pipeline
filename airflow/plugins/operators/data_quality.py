8c93185b716eaea95bb827dc02c1d43605c5bf4e
MustafaAlbahrani <75665113+MustafaAlbahrani@users.noreply.github.com>
2024-02-15 13:11:46 +0300

    Update data_quality.py

--- airflow/plugins/operators/data_quality.py
+++ airflow/plugins/operators/data_quality.py
@@ -6,29 +6,31 @@ class DataQualityOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 redshift_conn_id = "",
-                 tables = [],
+                 redshift_conn_id="",
+                 tables=[],
+                 expected_results={},
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        
         self.redshift_conn_id = redshift_conn_id
         self.tables = tables
-        
+        self.expected_results = expected_results
 
     def execute(self, context):
-        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)  
-        
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
         for table in self.tables:
-            records = redshift.get_records("SELECT COUNT(*) FROM {}".format(table))  
-            
+            records = redshift.get_records(f"SELECT COUNT(*) FROM {table}")
+
             if len(records) < 1 or len(records[0]) < 1:
-                self.log.error("{} returned no results".format(table))
-                raise ValueError("Data quality check failed. {} returned no results".format(table))
-                
+                self.log.error(f"{table} returned no results")
+                raise ValueError(f"Data quality check failed. {table} returned no results")
+
             num_records = records[0][0]
-            if num_records == 0:
-                self.log.error("No records present in destination table {}".format(table))
-                raise ValueError("No records present in destination {}".format(table))
-                
-            self.log.info("Data quality on table {} check passed with {} records".format(table, num_records))
+            expected_result = self.expected_results.get(table, 0)
+
+            if num_records != expected_result:
+                self.log.error(f"Data quality check failed for {table}. Expected {expected_result} records, but found {num_records}")
+                raise ValueError(f"Data quality check failed for {table}. Expected {expected_result} records, but found {num_records}")
+
+            self.log.info(f"Data quality on table {table} check passed with {num_records} records")
